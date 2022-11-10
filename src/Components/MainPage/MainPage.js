ba885d2da24598adc681387103ea86fa371aa338
William Hobson <will.hobson85@gmail.com>
2022-11-10 13:58:32 -0500

    Build tile framework

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -3,12 +3,11 @@ import './MainPage.css'
 
 const MainContainer = () => {
     
-return (
-  <div className='main-container'>
-    <h2>This will hold the searched words.</h2>
-  </div>
-)
-    
+  return (
+    <div className='main-container'>
+      <h2>This will hold the searched words.</h2>
+    </div>
+  )
 }
 
 export default MainContainer
