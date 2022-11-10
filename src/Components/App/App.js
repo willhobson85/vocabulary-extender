2be79fa6408fa4b8600a8dfaecbcc42e84bfd65b
William Hobson <will.hobson85@gmail.com>
2022-11-09 23:56:12 -0500

    Start writing main container

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import React, { Component } from 'react';
+import MainContainer from '../MainPage/MainPage';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
       
-    </div>
-  );
+    }
+  }
+
+  render() {
+    return (
+      <main className='home'>
+        <h1>This will contain the main container</h1>
+        <h2>Also the form</h2>
+        <MainContainer />
+      </main>
+    );
+  }
+
 }
 
 export default App;
