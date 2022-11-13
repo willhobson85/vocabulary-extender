2c1d81629ed4b7de0afba83797bc00803a08eacc
William Hobson <will.hobson85@gmail.com>
2022-11-13 16:37:23 -0500

    Remove strict mode

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,6 @@ import MainContainer from '../MainPage/MainPage';
 import SavedContainer from '../SavedPage/SavedPage';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Form from "../InputForm/InputForm"
 import { fetchData } from '../../apiCalls';
 import {
   BrowserRouter as Router,
@@ -17,10 +16,10 @@ class App extends Component {
     super()
     this.state = {
       wordList: [{
-        id: 1668366024888,
         noun: {
           syn: ["hello", "hullo", "howdy", "how-do-you-do", "Hawaii", "Hawai'i", "Aloha State", "HI", "American state", "greeting", "salutation"]
         },
+        id: 1668366024888,
         word: "hi"
       }],
       savedWords: [],
@@ -44,25 +43,23 @@ class App extends Component {
   }
 
   saveTile = (id) => {
-    const saveCard = this.state.wordList.filter(card => card.id === id)
-    this.setState({savedWords: [...this.state.savedWords, saveCard]})
+    const saveCard = this.state.wordList.filter(card => card.id === id).flat()
+    this.setState({savedWords: [...this.state.savedWords, saveCard[0]]})
   }
   
   render() {
+    console.log('saved words', this.state.savedWords)
+    console.log('word list', this.state.wordList)
     return (
       <main className='home'>
-        <Router>
+        {/* <Router> */}
           <Header searchWord={this.searchWord}/>
           <Switch>
-            <Route path="/savedWords">
-              <SavedContainer savedWords={this.state.savedWords} saveTile={this.saveTile} removeTile={this.removeTile} errorMessage={this.state.errorMessage} />
-            </Route>
-            <Route path="/">
-              <MainContainer wordList={this.state.wordList} saveTile={this.saveTile} removeTile={this.removeTile} errorMessage={this.state.errorMessage} />
-            </Route>
+            <Route exact path="/savedPage" render={() => <SavedContainer savedWords={this.state.savedWords} saveTile={this.saveTile} removeTile={this.removeTile} errorMessage={this.state.errorMessage} />} />
+            <Route path="/" render={() => <MainContainer wordList={this.state.wordList} saveTile={this.saveTile} removeTile={this.removeTile} errorMessage={this.state.errorMessage} />} />
           </Switch>
           <Footer />
-        </Router>
+        {/* </Router> */}
       </main>
     );
   }
