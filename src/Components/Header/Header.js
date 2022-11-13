7c9a29f855600ebd67d0ec7cf1ca97888176366b
William Hobson <will.hobson85@gmail.com>
2022-11-13 16:14:27 -0500

    Add link in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Form from "../InputForm/InputForm"
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = ({ searchWord }) => {
 
   return (
     <header className="app-head">
-      <button className="goToSaved">Saved Words</button>
+      <Link to="/savedPage">
+        <button className="goToSaved">Saved Words</button>
+      </Link>
       <Form className="search-box" searchWord={searchWord}/>
     </header>
   )
